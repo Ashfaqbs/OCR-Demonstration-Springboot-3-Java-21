f14774f904de315b98f7fbbe49925e37d0bbc454
Ashfaqbs <ash001x@gmail.com>
2023-12-10 19:49:23 +0530

    m1

---
+++ src/main/java/com/main/springboot/OcrDemonstrationSpringboot3CurdJ22Application.java
@@ -0,0 +1,13 @@
+package com.main.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrDemonstrationSpringboot3CurdJ22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrDemonstrationSpringboot3CurdJ22Application.class, args);
+	}
+
+}
